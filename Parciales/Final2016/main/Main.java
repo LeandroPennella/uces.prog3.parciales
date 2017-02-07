0c10f4192d4ac9bfe555e10a929f3cadc17e09a2
leandrop <leandropennella@yahoo.com.ar>
2017-02-07 13:53:22 -0300

    enunciado

--- Parciales/Final2016/main/Main.java
+++ Parciales/Final2016/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import java.io.FileReader;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		FileReader fr = new FileReader(file); 
 	}
 
 }
