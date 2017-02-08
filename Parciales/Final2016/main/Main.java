6a98481e722e3c1c9197722398625a7b5cb994ae
leandropennella <leandropennella@envy>
2017-02-07 21:52:40 -0300

    read

--- Parciales/Final2016/main/Main.java
+++ Parciales/Final2016/main/Main.java
@@ -1,13 +1,25 @@
 package main;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		FileReader fr = new FileReader(file); 
+		//abrir
+		String ArchivoConNumeros="numeros.txt";
+		try {
+			FileReader fr = new FileReader(ArchivoConNumeros);
+            int valor=fr.read();
+            while(valor!=-1){
+                System.out.print((char)valor);
+                valor=fr.read();
+            }
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
 	}
 
 }
