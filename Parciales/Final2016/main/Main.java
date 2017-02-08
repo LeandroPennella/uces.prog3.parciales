53d8ae5a8f096d21ec3cd020319d281ff7395bcb
leandropennella <leandropennella@envy>
2017-02-08 06:57:06 -0300

    numeros

--- Parciales/Final2016/main/Main.java
+++ Parciales/Final2016/main/Main.java
@@ -1,14 +1,23 @@
 package main;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 		//abrir
 		String ArchivoConNumeros="numeros.txt";
+
+	}
+	
+	private String leerArchivo(string nombreArchivo)
+	{
 		try {
 			FileReader fr = new FileReader(ArchivoConNumeros);
             int valor=fr.read();
@@ -20,6 +29,32 @@ public class Main {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} 
+	}
+	
+	private int[] extraerNumeros(String numerosString)
+	{
+	  String cadena="";
+	  BufferedReader b = new BufferedReader(fr);
+	  
+	  //extraigo string del archivo;
+	  try {
+		while((cadena = b.readLine())!=null) {
+	  }
+	} catch (IOException e) {
+		e.printStackTrace();
+	}
+	  
+	  //divido en string[] el string
+	  String numerosString[] = cadena.split(",");
+	  
+	  //convierto string[] en int[]
+	  int numeros[]={};
+	  for(int i = 0;i < numerosString.length;i++)
+	  {
+		  numeros[i] = Integer.parseInt(numerosString[i]);
+	  }
+	  return numeros;  
+
 	}
 
 }
